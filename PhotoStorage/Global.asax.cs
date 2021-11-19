1a35172bbb86827435365b845e0c5b62fdd68cae
Muhammed <muhammedmansuri6@gmail.com>
2021-11-19 17:57:15 +0200

    Add Project

--- PhotoStorage/Global.asax.cs
+++ PhotoStorage/Global.asax.cs
@@ -12,6 +12,7 @@ namespace PhotoStorage
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            //()
         }
     }
 }
